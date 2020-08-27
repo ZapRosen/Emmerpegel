5336392d4cc994ba5515fb938a2db95f814fcd8b
Richard B.RiX <44611385+ZapRosen@users.noreply.github.com>
2020-08-27 12:45:18 +0200

    Add files via upload

---
+++ python/webscrape-title.py
@@ -0,0 +1,14 @@
+import re
+from urllib.request import urlopen
+
+url = "https://zaprosen.github.io/"
+page = urlopen(url)
+html = page.read().decode("utf-8")
+
+muster = "<title.*?>.*?</title.*?>"
+match_results = re.search(muster, html, re.IGNORECASE)
+title = match_results.group()
+title = re.sub("<.*?>", "", title) # Remove HTML tags
+
+print(title)
+
